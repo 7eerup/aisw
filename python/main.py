4218e97dd2c559fafd2d50b5bd5032c49151a966
7eerup <7eerup@gmail.com>
2026-04-01 18:31:44 +0900

    path

---
+++ python/main.py
@@ -0,0 +1,20 @@
+import os
+
+path = "/Users/7eerup/OrbStack/ubuntu/home/claude/aisw/python/data/hello.txt"
+
+with open(path, "r", encoding="utf-8") as f:
+    print("파일 내용:", f.read())
+
+print("\n전체 경로  :", os.path.abspath(path))
+print("폴더 경로  :", os.path.dirname(path))
+print("파일 이름  :", os.path.basename(path))
+print("현재 위치  :", os.getcwd())
+
+
+
+# import os
+
+# with open("./data/hello.txt", "r", encoding="utf-8") as f:
+#     print("파일 내용:", f.read())
+
+# print("현재 위치 :", os.getcwd())
