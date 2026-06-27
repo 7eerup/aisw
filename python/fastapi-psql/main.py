545efe5e523ab62bcb38ae81855c68ce7bbb7028
7eerup <7eerup@gmail.com>
2026-06-27 18:30:47 +0900

    feat: configure session middleware authentication
    - SessionMiddleware
    - python-dotenv
    - itsdangerous

--- python/fastapi-psql/main.py
+++ python/fastapi-psql/main.py
@@ -1,17 +1,28 @@
+import os
+
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
+from dotenv import load_dotenv
 
 from database import Base, engine
 from models.memo import Memo
 from routers.memo_router import router as memo_router
 
-Base.metadata.create_all(bind=engine)
+load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=os.getenv("SECRET_KEY"),
+)
 
 templates = Jinja2Templates(directory="templates")
 
 app.include_router(memo_router)
+
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
